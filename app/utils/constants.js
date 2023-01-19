cc23eb5008337274ad5c6dc002e1316f472c7a3c
MrChanghao <lnl2048@gmail.com>
2023-01-19 18:03:32 +0900

    url 변경

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,5 +2,4 @@ export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
 
-export const API_URL =
-  'http://ec2-3-38-209-251.ap-northeast-2.compute.amazonaws.com:8080/inference';
+export const API_URL = 'https://price-back.algorithmlabs.io/inference';
